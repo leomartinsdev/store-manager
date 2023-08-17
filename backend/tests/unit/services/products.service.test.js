b198f8101ace921acaf7eaf86c42ffc67bdd4787
Leonardo Martins <leonardogustavo@outlook.com>
2023-08-17 11:09:00 -0300

    Adicionei os testes do service para aprender

--- backend/tests/unit/services/products.service.test.js
+++ backend/tests/unit/services/products.service.test.js
@@ -1,6 +1,26 @@
-// const chai = require('chai');
-// const sinon = require('sinon');
-// const sinonChai = require('sinon-chai');
+const chai = require('chai');
+const sinon = require('sinon');
 
-// const { expect } = chai;
-// chai.use(sinonChai);
+const { expect } = chai;
+
+const { productsModel } = require('../../../src/models');
+const { productsService } = require('../../../src/services');
+const { allProductsFromModel, productFromModel } = require('../mocks/products.mock');
+
+describe('Testando o products.service:', function () {
+  it('Buscando todos os produtos', async function () {
+    sinon.stub(productsModel, 'findAll').resolves(allProductsFromModel);
+    const responseService = await productsService.findAll();
+    expect(responseService.status).to.equal('SUCCESSFUL');
+    expect(responseService.data).to.deep.equal(allProductsFromModel);
+  });
+
+  it('Buscando apenas um produto', async function () {
+    sinon.stub(productsModel, 'findById').resolves(productFromModel);
+    const searchedId = 1;
+
+    const responseService = await productsService.findById(searchedId);
+    expect(responseService.status).to.equal('SUCCESSFUL');
+    expect(responseService.data).to.deep.equal(productFromModel);
+  });
+});
